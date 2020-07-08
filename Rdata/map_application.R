04d4049f6511ae7958e9c56d9ddf42829a14ebcf
unknown <JungRyung>
2020-07-08 12:30:32 +0900

    update lec

--- Rdata/map_application.R
+++ Rdata/map_application.R
@@ -54,3 +54,46 @@ qmap(location = center,
              shape='*',
              color='red',
              stroke=18,size=10)
+
+### 서울시의 대학목록 추출하기
+url="https://namu.wiki/w/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C%EC%9D%98%20%EB%8C%80%ED%95%99%EA%B5%90%20%EB%AA%A9%EB%A1%9D"
+
+hdoc=read_html(url,encoding = "UTF-8")
+df=hdoc%>%
+  html_nodes(".wiki-paragraph a")%>%
+  html_text()
+head(df,50)
+univ = ifelse(str_detect(df,pattern = "대학교"),df,"")
+univ
+kk=univ%>%
+  data.frame()
+kk=Filter(function(x){nchar(x)>=5},univ)
+kk
+univName=kk[2:28]
+univName
+univCord=geocode(location=univName,
+                 output='latlon',
+                 source='google')
+univDF=data.frame(univ=univName,
+                  lon=univCord$lon,
+                  lat=univCord$lat)
+univDFna=na.omit(univDF)
+univDFna
+center = c(mean(x=univDFna$lon),mean(x=univDFna$lat))
+center
+
+qmap(location = center,
+     zoom=12,
+     maptype='satellite',
+     source='google') +
+  geom_point(data=univDFna,
+             aes(x=lon,y=lat),
+             shape='*',
+             color='red',size=6) +
+  geom_text(data=univDFna,
+            aes(x=lon,y=lat,label=univ),
+            color='green',hjust=0.5,
+            vjust=-0.1,
+            size=3,
+            fontface='bold',
+            family='NanumGodic')
